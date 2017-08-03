e862fde19abab487340428c6d22a8ea2922aafb8
AaronChou <993001803@qq.com>
2017-08-03 16:09:34 +0800

    Create tensorboard1.py

---
+++ tensorflow/TensorBoard/tensorboard1.py
@@ -0,0 +1,64 @@
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+from __future__ import print_function
+import tensorflow as tf
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    # 添加layer的函数
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+        # 是否有激活函数
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b, )
+        return outputs
+
+
+# 定义输入inputs
+with tf.name_scope('inputs'):
+    # 定义inputs中的x_input，y_input
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+
+# 添加中间层l1
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# l2 = add_layer(l1, 10, 10, activation_function=tf.nn.relu)
+# 添加输出层prediction
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# 计算真实数据和预测的误差
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                        reduction_indices=[1]))
+
+# train使用梯度下降最小化损失loss
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# 获得Session
+sess = tf.Session()
+
+# tf.train.SummaryWriter soon be deprecated, use following
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:  # tensorflow version < 0.12
+    writer = tf.train.SummaryWriter('logs/', sess.graph)
+else: # tensorflow version >= 0.12
+    writer = tf.summary.FileWriter("logs/", sess.graph)
+
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess.run(init)
+
+# direct to the local dir and run this in terminal:
+# $ tensorboard --logdir=logs
