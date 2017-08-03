050a18ce20977c4de7364d395d92d328f066dae7
AaronChou <993001803@qq.com>
2017-08-03 16:50:25 +0800

    Update tensorboard1.py

--- tensorflow/TensorBoard/tensorboard1.py
+++ tensorflow/TensorBoard/tensorboard1.py
@@ -1,17 +1,26 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
 """
 Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
 """
 from __future__ import print_function
 import tensorflow as tf
+import numpy as np
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
     # 添加layer的函数
-    with tf.name_scope('layer'):
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope(layer_name):
         with tf.name_scope('weights'):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            tf.summary.histogram(layer_name + '/weights', Weights)
         with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.summary.histogram(layer_name + '/biases', biases)
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
         # 是否有激活函数
@@ -19,46 +28,51 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b, )
-        return outputs
+        tf.summary.histogram(layer_name + '/outputs', outputs)
+    return outputs
 
+
+# 产生数据
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
 
 # 定义输入inputs
 with tf.name_scope('inputs'):
-    # 定义inputs中的x_input，y_input
-    xs = tf.placeholder(tf.float32, [1024, 1], name='x_input')
-    ys = tf.placeholder(tf.float32, [1024, 1], name='y_input')
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
 # 添加中间层l1
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-# l2 = add_layer(l1, 10, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # 添加输出层prediction
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 # 计算真实数据和预测的误差
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                                         reduction_indices=[1]))
-
+    tf.summary.scalar('loss', loss)
 # train使用梯度下降最小化损失loss
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
 # 获得Session
 sess = tf.Session()
+merged = tf.summary.merge_all()
 
-# tf.train.SummaryWriter soon be deprecated, use following
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:  # tensorflow version < 0.12
-    writer = tf.train.SummaryWriter('logs/', sess.graph)
-else: # tensorflow version >= 0.12
-    writer = tf.summary.FileWriter("logs/", sess.graph)
+writer = tf.summary.FileWriter("logs/", sess.graph)
 
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
+# 初始化
+init = tf.global_variables_initializer()
 sess.run(init)
 
+for i in range(1000):
+    # 每一步放入数据训练。
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        # 每50记录所有的summary
+        result = sess.run(merged,
+                          feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result, i)
+
 # direct to the local dir and run this in terminal:
-# $ tensorboard --logdir=logs
+# $ tensorboard --logdir logs
