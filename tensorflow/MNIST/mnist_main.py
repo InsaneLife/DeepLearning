4b60fe40587b96ba2a351c1b3cb832d03c2071ab
AaronChou <993001803@qq.com>
2017-08-30 18:47:05 +0800

    Update mnist_main.py

--- tensorflow/MNIST/mnist_main.py
+++ tensorflow/MNIST/mnist_main.py
@@ -2,6 +2,7 @@
 """
 author=Aaron
 python=3.5
+TensorFlow=1.2.1
 """
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
@@ -36,8 +37,7 @@ def max_pool_2x2(x):
 
 # 读取数据
 mnist = input_data.read_data_sets("D:/data/minist/", one_hot=True)
-# seesion初始化变量
-sess = tf.InteractiveSession()
+
 # 为输入图像和目标输出类别创建节点，来开始构建计算图。x和y是占位符
 x = tf.placeholder("float", shape=[None, 784])  # 784＝28x28
 y_ = tf.placeholder("float", shape=[None, 10])
@@ -89,15 +89,33 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)  # 3333333333 
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 # 计算出匹配结果correct_prediction的平均值为，如[1,0,1,1]为0.75
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
-for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-# 输出最终模型在测试集上的准确率
-print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+# 创建一个Saver对象，选择性保存变量或者模型。
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(20000):
+        batch = mnist.train.next_batch(50)
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g" % (i, train_accuracy))
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    # 输出最终模型在测试集上的准确率
+    print("test accuracy %g" % accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+    # 保存模型到model.ckpt
+    save_path = saver.save(sess, "model/model.ckpt")
+    print("Model saved in file: ", save_path)
+
+# with tf.Session() as sess:
+#     # 读取模型
+#     saver.restore(sess, "model/model.ckpt")
+#     print("Model restored.")
+#
+#     # 输出最终模型在测试集上的准确率
+#     print("test accuracy %g" % accuracy.eval(feed_dict={
+#         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
